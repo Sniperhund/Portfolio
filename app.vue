9952714784e07e984278fd4fe3244c0935c24951
Lucas Täkker <lucastakker@gmail.com>
2024-02-02 23:16:17 +0100

    Removed Vercel Speed Insights and replaced it with Vercel Analystics

--- app.vue
+++ app.vue
@@ -1,11 +1,5 @@
-<script setup>
-import { SpeedInsights } from "@vercel/speed-insights/next";
-</script>
-
 <template>
-  <div>
-    <NuxtPage />
-
-    <SpeedInsights />
-  </div>
+	<div>
+		<NuxtPage />
+	</div>
 </template>
